6d61754cf72dc9c9a627f77105d1672a33e2de1d
alnomancontact <alnoman.contact@gmail.com>
2026-07-22 00:00:43 +0600

    Update main_test_vrt.py

--- support/Noman_Super Resolution/main_test_vrt.py
+++ support/Noman_Super Resolution/main_test_vrt.py
@@ -1,6 +1,6 @@
 !python main_test_vrt.py \
   --task 001_VRT_videosr_bi_REDS_6frames \
-  --folder_lq testsets/synthetic_disaster \
+  --folder_lq testsets/custom_disaster \
   --tile 40 128 128 \
   --tile_overlap 2 20 20 \
   --num_workers 2 \
