717f2090215a6775848fce84c8de319ed7eb043b
Ridita Afrin <ridita0177@gmail.com>
2026-07-21 19:06:09 +0600

    Delete From Pixel to Semantic/From-Pixels-to-Semantic directory

--- From Pixel to Semantic/From-Pixels-to-Semantic/train.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load a pretrained model
-model = YOLO('yolo11n.pt')  # You can choose yolo11n.pt, yolo11s.pt, yolo11m.pt, etc.
-
-# Train the model 
-results = model.train(data='code/xview-buildings.yaml', epochs=50, imgsz=640, save=True)
